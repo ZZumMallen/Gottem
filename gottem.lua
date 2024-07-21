2978b7f121015d89d256db44e103320e30965c5c
Zach <91436608+ZZumMallen@users.noreply.github.com>
2024-07-20 20:28:30 -0500

    chat works function works

--- gottem.lua
+++ gottem.lua
@@ -4,18 +4,30 @@
 local addonName, core = ...;
 local G = core.C
 
-function OnEvent(self, event, arg1, ...)
-    if event == "CHAT_MSG_SAY" and arg1 == "test" then
+local f = CreateFrame("Frame")
+f:RegisterEvent("CHAT_MSG_SAY")
+f:RegisterEvent("CHAT_MSG_ADDON")
+f:SetScript("OnEvent", function (self, event, arg1, ...)
+    if event == "CHAT_MSG_SAY" and arg1 == "go" then
         X = GetUnitName("Target")
-        if X == nil or not UnitIsPlayer("target") then
+        if X == nil then
+                G.Result = print("you need to target something")
             return
         else
-            for _, j in ipairs(G.tList) do
-                if j == X then
-                    G.isFound = true
-                    G.foundTarget = X
-                end                               
-            end            
+            if UnitIsPlayer("Target") then 
+                for _, j in ipairs(G.tList) do                    
+                    if j == X then
+                        print(X)
+                        G.isFound = true
+                        G.foundTarget = X
+                        G.Result = X
+                    end                                 
+                end
+            else
+                local thing = UnitCreatureFamily("target")
+                print("That's a " .. thing .. " you idiot!")
+                return
+            end          
         end
    
         ---@class mapID:number
@@ -25,12 +37,14 @@ function OnEvent(self, event, arg1, ...)
         G.locName = C_Map.GetMapInfo(mapID).name
 
         if pos == nil then
+            G.locX = 0
+            G.locY = 0            
             return 
         else
             G.locX = math.ceil(pos.x * 10000) / 100
             G.locY = math.ceil(pos.y * 10000) / 100
-        end       
+        end
+        print(G.Result, G.isFound, G.foundTarget, G.locName, G.locX, G.locY)
     end
-end
-
+end)
 

2e8b4b506a7624f010bf892bbf4f57e873f62e8a
Zach <91436608+ZZumMallen@users.noreply.github.com>
2024-07-21 19:48:55 -0500

    combinating everythang

--- gottem.lua
+++ gottem.lua
@@ -4,47 +4,41 @@
 local addonName, core = ...;
 local G = core.C
 
-local f = CreateFrame("Frame")
-f:RegisterEvent("CHAT_MSG_SAY")
-f:RegisterEvent("CHAT_MSG_ADDON")
-f:SetScript("OnEvent", function (self, event, arg1, ...)
-    if event == "CHAT_MSG_SAY" and arg1 == "go" then
-        X = GetUnitName("Target")
-        if X == nil then
-                G.Result = print("you need to target something")
-            return
-        else
-            if UnitIsPlayer("Target") then 
-                for _, j in ipairs(G.tList) do                    
-                    if j == X then
-                        print(X)
-                        G.isFound = true
-                        G.foundTarget = X
-                        G.Result = X
-                    end                                 
-                end
-            else
-                local thing = UnitCreatureFamily("target")
-                print("That's a " .. thing .. " you idiot!")
-                return
-            end          
-        end
-   
-        ---@class mapID:number
-        local mapID = C_Map.GetBestMapForUnit(X)        
-        local pos = C_Map.GetPlayerMapPosition(mapID, X);        
+-- function Verify_Storez()
+--     local X = GetUnitName("Target")
+--     if X == nil then
+--             G.Result = print("you need to target something")
+--         return
+--     else
+--         if UnitIsPlayer("Target") then 
+--             for _, j in ipairs(G.tList) do                    
+--                 if j == X then
+--                     print(X)
+--                     G.isFound = true
+--                     G.foundTarget = X
+--                     G.Result = X
+--                 end                                 
+--             end
+--         else
+--             local thing = UnitCreatureFamily("target")
+--             print("That's a " .. thing .. " you idiot!")
+--             return
+--         end          
+--     end
 
-        G.locName = C_Map.GetMapInfo(mapID).name
+--     ---@class mapID:number
+--     local mapID = C_Map.GetBestMapForUnit(X)        
+--     local pos = C_Map.GetPlayerMapPosition(mapID, X);        
 
-        if pos == nil then
-            G.locX = 0
-            G.locY = 0            
-            return 
-        else
-            G.locX = math.ceil(pos.x * 10000) / 100
-            G.locY = math.ceil(pos.y * 10000) / 100
-        end
-        print(G.Result, G.isFound, G.foundTarget, G.locName, G.locX, G.locY)
-    end
-end)
+--     G.locName = C_Map.GetMapInfo(mapID).name
 
+--     if pos == nil then
+--         G.locX = 0
+--         G.locY = 0            
+--         return 
+--     else
+--         G.locX = math.ceil(pos.x * 10000) / 100
+--         G.locY = math.ceil(pos.y * 10000) / 100
+--     end
+--     print(G.Result, G.isFound, G.foundTarget, G.locName, G.locX, G.locY)
+-- end
--- messager.lua
+++ messager.lua
@@ -1,30 +1,61 @@
 local addonName, core = ...;
 local G = core.C
-local messagePrefix = "ope_gottem"
-
+G.triggerPrefix = "ope_gottem"
+G.messagePrefix = "sneak_right_past_ya_there"
 
 SLASH_ZGG1 = "/gottem"
-SlashCmdList["ZGG"] = function() C_ChatInfo.SendAddonMessage(messagePrefix, "go", "Guild") end;
+SlashCmdList["ZGG"] = function() C_ChatInfo.SendAddonMessage(G.triggerPrefix, "go", "Guild") end;
 
 local f = CreateFrame("FRAME")
 f:RegisterEvent("ADDON_LOADED")
 f:RegisterEvent("CHAT_MSG_ADDON")
 f:SetScript("OnEvent", function(self, event, arg1, arg2, ...)
     if event == "ADDON_LOADED" and arg1 == addonName then
-        C_ChatInfo.RegisterAddonMessagePrefix(messagePrefix)
+        C_ChatInfo.RegisterAddonMessagePrefix(G.triggerPrefix)
         print("Messager Loaded")
     elseif
-        event == "CHAT_MSG_ADDON" and arg1 == messagePrefix and arg2 == "go" then
-            message("WE DID IT")
-    end    
-end)
+        event == "CHAT_MSG_ADDON" and arg1 == G.triggerPrefix and arg2 == "go" then
+        Verify_Store()
 
+      
+        end
+end)   
 
+function Verify_Store()
+    local X = GetUnitName("Target")
+    if X == nil then
+        G.Result = print("you need to target something")
+        return
+    else
+        if UnitIsPlayer("Target") then
+            for _, j in ipairs(G.tList) do
+                if j == X then
+                    print(X)
+                    G.isFound = true
+                    G.foundTarget = X
+                    G.Result = X
+                end
+            end
+        else
+            local thing = UnitCreatureFamily("target")
+            print("That's a " .. thing .. " you idiot!")
+            return
+        end
+    end
 
+    ---@class mapID:number
+    local mapID = C_Map.GetBestMapForUnit(X)
+    local pos = C_Map.GetPlayerMapPosition(mapID, X);
 
+    G.locName = C_Map.GetMapInfo(mapID).name
 
-
-
-
-
-
+    if pos == nil then
+        G.locX = 0
+        G.locY = 0
+        return
+    else
+        G.locX = math.ceil(pos.x * 10000) / 100
+        G.locY = math.ceil(pos.y * 10000) / 100
+    end
+    print(G.Result, G.isFound, G.foundTarget, G.locName, G.locX, G.locY)
+end
